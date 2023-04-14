a420309aaeb32d4951b019156b37332e176c796f
Jorge Ortiz <jeortiz.quan@gmail.com>
2023-04-14 06:47:39 +0200

    Initial phaser 3 setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Phaser  from 'phaser';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,59 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'in-about-the-word';
+  phaserGame: Phaser.Game;
+  config: Phaser.Types.Core.GameConfig;
+  constructor() {
+    this.config = {
+      type: Phaser.AUTO,
+      height: 600,
+      width: 800,
+      scene: [MainScene],
+      parent: 'gameContainer',
+      physics: {
+        default: 'arcade',
+        arcade: {
+          gravity: { y: 100 }
+        }
+      }
+    };
+    this.phaserGame = new Phaser.Game(this.config);
+  }
+}
+
+class MainScene extends Phaser.Scene {
+  constructor() {
+    super({ key: 'main' });
+  }
+  create() {
+    console.log('create method');
+    this.add.image(400, 300, 'sky');
+
+    let particles = this.add.particles('red');
+
+    let emitter = particles.createEmitter({
+        speed: 100,
+        scale: { start: 1, end: 0 },
+        blendMode: 'ADD'
+    });
+
+    let logo = this.physics.add.image(400, 100, 'logo');
+
+    logo.setVelocity(100, 200);
+    logo.setBounce(1, 1);
+    logo.setCollideWorldBounds(true);
+
+    emitter.startFollow(logo);
+  }
+  preload() {
+    console.log('preload method');
+    this.load.setBaseURL('http://labs.phaser.io');
+
+    this.load.image('sky', 'assets/skies/space3.png');
+    this.load.image('logo', 'assets/sprites/phaser3-logo.png');
+    this.load.image('red', 'assets/particles/red.png');
+  }
+  override update() {
+    console.log('update method');
+  }
 }
